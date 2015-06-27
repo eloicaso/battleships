dcfcb29d43c10fe43334b6b00717f4c4aa350e43
Eloi Carbo <eloicaso@gmail.com>
2015-06-28 00:38:00 +0200

    Finish all the logic behind the game. Check TODO to see pending tasks

--- logic/battleship.js
+++ logic/battleship.js
@@ -1,5 +1,6 @@
 /*
 *   Battleship plain JS logic
+*   Eloi Carbo 2015 - MIT License
 */
 
 // Specify game CONSTANTS
@@ -14,27 +15,53 @@ HIT_SQ = 'X'
 MISS_SQ = 'M'
 LOOPS_MAX = 5000
 
-//Hint values ('sugar' to avoid .length calls)
+/* Begining of hint values */
+// 'sugar' to avoid .length calls
 var row_length = ROW_NUMBER - 1
 var column_length = COLUMN_NUMBER - 1
+// # of: ship squares destroyed, pending, missed, hit and accuracy achieved
+var destroyedSq, aliveSq, missedSq, accuracy
+/* End of hint Values */
 
-// Create the user's board
+/* Create the game and shadow boards:
+* gameBoard will contain all hits/misses from user's interaction
+* shadowBoard will contain the randomized distribution of ships
+*/
 var gameBoard = []
+var shadowBoard = []
 
 
-// Initialize the (ROW_NUMBER * COLUMN_NUMBER) Board to water/empty value
-for (var x = 0; x < ROW_NUMBER; x++) {
-  gameBoard[x] = []
-  for (var y = 0; y<COLUMN_NUMBER; y++) {
-    gameBoard[x][y] = WATER_SQ
+/*
+* initializeHintValues ( number destroyed, number alive, number missed,
+* number accuracy)
+* This function initializes to 0 the hint values
+*/
+function initializeHintValues (destroyed, alive, mised, accuracy) {
+  destroyed, alive, mised, accuracy = 0
+}
+
+
+/*
+* initializeBoard ( array[][] board )
+* Initialize the (ROW_NUMBER * COLUMN_NUMBER) Board to water/empty value
+*/
+function initializeBoard(board) {
+  for (var x = 0; x < ROW_NUMBER; x++) {
+    board[x] = []
+    for (var y = 0; y<COLUMN_NUMBER; y++) {
+      board[x][y] = WATER_SQ
+    }
   }
 }
 
-/* Clone the water/empty board in order to obtain the shadow board
-* (which will include the random WATER/SHIP information values).
-* Using slice(0) to improve performance [TODO: Check this].
- */
-var shadowBoard = gameBoard.slice(0)
+/*
+* initBoards ( )
+* This function sets to WATER_SQ value all the game/shadow GRIDs
+*/
+function initBoards() {
+  initializeBoard(gameBoard)
+  initializeBoard(shadowBoard)
+}
 
 
 /*
@@ -121,36 +148,80 @@ function setShip(size) {
 
 /*
 * printDistribution ( )
-* This function only shows the created map in a log style. Debuggin purpose only
+* This functions shows the created board in a log style. Debuggin purpose only
 * Return -
 */
-function printDistribution() {
+function printShadow() {
   console.log(shadowBoard)
+}
+function printGame() {
+  console.log(gameBoard)
 }
 
 
 /*
 * randomDistribution ( {number Battleship, number Destroyer} )
-* This function is the responsible to distribute all the SHIPS in the shadowBoard.
+* This function is the responsible to distribute all the SHIPS in the shadowBoard
 * Return - Boolean
 */
 function randomDistribution () {
-/* TODO: if this function returns false means that something went wrong (maybe add a maximum number of tries in case CONSTANT values are not fittable) */
+  /* TODO: if this function returns false means that something went wrong (maybe add a maximum number of tries in case CONSTANT values are not fittable in the given GRID) */
   var battleType = SHIPS.Battleship
   var destrType = SHIPS.Destroyer
 
   // Set first biggest ship
   if(battleType > 0) {
     for (var i = 0; i < battleType; i++) {
-      if(!setShip(BATTLESHIP)) return false
+      if (!setShip(BATTLESHIP)) return false
     }
   }
 
   if(destrType > 0) {
     for (var i = 0; i < destrType; i++) {
-      if(!setShip(DESTROYER)) return false
+      if (!setShip(DESTROYER)) return false
     }
   }
-  printDistribution()
+  return true
+}
+
+
+/*
+* checkAndHit ( number x, number y)
+* This function uses x and y values to check shadowBoard contents in that
+* position and print it in the gameBoard
+*/
+function checkAndHit(x, y) {
+  if (gameBoard[x][y] !== WATER_SQ) return false
+  else {
+    if (shadowBoard[x][y] === WATER_SQ) gameBoard[x][y] = MISS_SQ
+    else gameBoard[x][y] = HIT_SQ
+  }
+  return true
+}
+
+
+/*
+* hitSquare ( number x, number y)
+* This function uses x and y values to check shadowBoard contents in that
+* position and print it in the gameBoard
+* Returns true if the position is inside the grid AND not previously
+* hit by another hitSquare function call
+*/
+function hitSquare(x, y) {
+  if (x > 9 || y > 9) return false
+  if (!checkAndHit(x, y)) return false
+  return true
+}
+
+
+/*
+* initializeGame ( )
+* This function starts a Battleship game by initializing values to 0/water * and then creating the randomized ships distribution
+* Returns true if the game has been successfully created
+*/
+function initializeGame () {
+  initializeHintValues()
+  initBoards()
+  randomDistribution()
   return true
 }
